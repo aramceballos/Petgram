8a01629a5c5869396a1d2de5e513598d8a3a1fd8
Aram Ceballos <aramgonzalez12@hotmail.com>
2020-02-17 23:06:40 -0700

    Protecting Routes

--- src/App.js
+++ src/App.js
@@ -4,11 +4,18 @@ import { GlobalStyle } from './styles/GlobalStyles';
 import { Logo } from './components/Logo';
 import { Home } from './pages/Home';
 import { Detail } from './pages/Detail';
+import { Favs } from './pages/Favs';
+import { User } from './pages/User';
+import { NotRegisteredUser } from './pages/NotRegisteredUser';
 import { NavBar } from './components/NavBar';
 
 export const App = () => {
   const urlParams = new window.URLSearchParams(window.location.search);
   const detailId = urlParams.get('detail');
+
+  const UserLogged = ({ children }) => {
+    return children({ isAuth: false });
+  };
 
   return (
     <>
@@ -19,6 +26,21 @@ export const App = () => {
         <Home path="/pet/:categoryId" />
         <Detail path="/detail/:detailId" />
       </Router>
+      <UserLogged>
+        {({ isAuth }) =>
+          isAuth ? (
+            <Router>
+              <Favs path="/favs" />
+              <User path="/user" />
+            </Router>
+          ) : (
+            <Router>
+              <NotRegisteredUser path="/favs" />
+              <NotRegisteredUser path="/user" />
+            </Router>
+          )
+        }
+      </UserLogged>
       <NavBar />
     </>
   );
