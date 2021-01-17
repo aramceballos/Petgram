c6fe3861ba60ad61c8dd14a6489005af3c98e44a
Aram Ceballos <aramgonzalez12@hotmail.com>
2021-01-17 16:07:00 -0700

    token name changed

--- src/frontend/pages/Login.js
+++ src/frontend/pages/Login.js
@@ -92,7 +92,7 @@ const Login = () => {
         document.cookie = `email=${data.user.email}`;
         document.cookie = `name=${data.user.name}`;
         document.cookie = `id=${data.user.id}`;
-        document.cookie = `petgramId=${data.token}`;
+        // document.cookie = `petgramId=${data.token}`;
         window.location.href = '/';
       })
       .catch(() => {
